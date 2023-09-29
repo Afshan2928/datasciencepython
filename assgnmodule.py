282eb54bc6e037b2f1450c542472d082be59df08
Afshan2928 <145640851+Afshan2928@users.noreply.github.com>
2023-09-29 21:14:12 +0530

    Add files via upload

--- assgnmodule.py
+++ assgnmodule.py
@@ -21,3 +21,31 @@
 # y=int(input("enter second integer"))
 # addresult=operator.add(x,y)
 # print("result",addresult)
+
+#3. Write Programs to implement time module (includes formatting) ?
+# import time
+
+# # Get the current time in seconds since the epoch (January 1, 1970)
+# current_time = time.time()
+# print(f"Current time in seconds since the epoch: {current_time}")
+
+# # Convert the current time to a struct_time object
+# current_struct_time = time.localtime(current_time)
+# print(f"Current time as a struct_time object: {current_struct_time}")
+
+# # Format the current time as a string
+# formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_struct_time)
+# print(f"Formatted time: {formatted_time}")
+
+# # Sleep for 2 seconds
+# print("Sleeping for 2 seconds...")
+# time.sleep(2)
+# print("Woke up!")
+
+# # Measure the time it takes to execute a code block
+# start_time = time.time()
+# for i in range(1000000):
+#     pass
+# end_time = time.time()
+# elapsed_time = end_time - start_time
+# print(f"Time taken to execute the loop: {elapsed_time} seconds")
